d81da59a23cd2c65f70eaad079902d1ce5d9a9b4
Nongoogle <essamsleem9@gmail.com>
2026-09-22 18:48:15 +0300

    Part02 Question07

--- Program.cs
+++ Program.cs
@@ -364,6 +364,100 @@
             }
         }
 
+        public class DeliveryCenter
+        {
+            public string CenterName { get; set; }
+            public Driver Driver { get; set; }
+            private Shipment[] shipments;
+
+            public DeliveryCenter(string centerName)
+            {
+                CenterName = centerName;
+                shipments = new Shipment[20];
+            }
+
+            public Shipment this[int index]
+            {
+                get
+                {
+                    if (index >= 0 && index < shipments.Length)
+                        return shipments[index];
+
+                    return default;
+                }
+
+                set
+                {
+                    if (index >= 0 && index < shipments.Length)
+                        shipments[index] = value;
+                }
+            }
+
+            public Shipment this[string trackingCode]
+            {
+                get
+                {
+                    for (int i = 0; i < shipments.Length; i++)
+                    {
+                        if (shipments[i].TrackingCode == trackingCode)
+                            return shipments[i];
+                    }
+
+                    return default;
+                }
+            }
+
+            public bool AddShipment(Shipment shipment)
+            {
+                for (int i = 0; i < shipments.Length; i++)
+                {
+                    if (shipments[i].TrackingCode == null)
+                    {
+                        shipments[i] = shipment;
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            public bool RemoveShipment(string trackingCode)
+            {
+                for (int i = 0; i < shipments.Length; i++)
+                {
+                    if (shipments[i] != null && shipments[i].TrackingCode == trackingCode)
+                    {
+                        shipments[i] = null;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public void PrintAllShipments()
+            {
+                for (int i = 0; i < shipments.Length; i++)
+                {
+                    if (shipments[i] != null)
+                    {
+                        shipments[i].PrintShipment();
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            public void PrintTrackingStatuses()
+            {
+                for (int i = 0; i < shipments.Length; i++)
+                {
+                    if (shipments[i] != null)
+                    {
+                        ITrackable shipment = shipments[i];
+                        Console.WriteLine(shipment.GetTrackingStatus());
+                    }
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
